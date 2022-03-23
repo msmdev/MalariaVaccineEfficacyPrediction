18ef2d000838b9597d145abf0145cf8d35181786
Bernhard Reuter <bernhard-reuter@gmx.de>
2022-03-23 17:29:50 +0100

    Temporary mods in multitask runner.

--- bin/run_rgscv_multitask.sh
+++ bin/run_rgscv_multitask.sh
@@ -6,10 +6,10 @@ conda activate malaria_env
 maindir='/home/breuter/MalariaVaccineEfficacyPrediction/results/multitaskSVM'
 data_maindir='/home/breuter/MalariaVaccineEfficacyPrediction/data/precomputed_multitask_kernels'
 #combinations=('SPP' 'SPR' 'SRP' 'SRR' 'RPP' 'RPR' 'RRP' 'RRR')
-combinations=('RPP' 'RPR' 'RRP' 'RRR')
+combinations=('SPP' 'SPR' 'SRP' 'SRR')
 
 #for dataset in 'whole' 'selective'; do
-for dataset in 'whole'; do
+for dataset in 'selective'; do
     if [ "$dataset" = 'whole' ]; then
         identifier='kernel_matrix'
     elif [ "$dataset" = 'selective' ]; then
@@ -27,7 +27,7 @@ for dataset in 'whole'; do
             data_dir="${data_maindir}/${scaling}"
             mkdir "${ana_dir}"
             cd "${ana_dir}" || { echo "Couldn't cd into ${ana_dir}"; exit 1; }
-            cp /home/breuter/MalariaVaccineEfficacyPrediction/bin/rgscv_multitask.py . || { echo "cp /home/breuter/MalariaVaccineEfficacyPrediction/bin/rgscv_multitask_whole.py . failed"; exit 1; }
+            cp /home/breuter/MalariaVaccineEfficacyPrediction/bin/rgscv_multitask.py . || { echo "cp /home/breuter/MalariaVaccineEfficacyPrediction/bin/rgscv_multitask.py . failed"; exit 1; }
 
             python -u rgscv_multitask.py --analysis-dir "${ana_dir}" --data-dir "${data_dir}" --combination "${combination}" --identifier "${identifier}" 1> "${out}" 2> "${err}"
         done
