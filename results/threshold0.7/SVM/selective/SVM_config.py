3e0e579250b0a446b840505392fa6a9fd26e1402
Bernhard Reuter <bernhard-reuter@gmx.de>
2022-09-13 12:02:37 +0200

    Add RNCV results.

---
+++ results/threshold0.7/SVM/selective/SVM_config.py
@@ -0,0 +1,25 @@
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from typing import Any, Dict, List
+import numpy as np
+from source.config import seed
+
+# Set up grid of parameters to optimize over
+# 3*3=9 points
+param_grid: Dict[str, List[Any]] = {
+    'svc__gamma': 10.0 ** np.arange(-6, 6),
+    'svc__C': 10.0 ** np.arange(-4, 5),
+}
+
+estimator = make_pipeline(
+    StandardScaler(
+        with_mean=True,
+        with_std=True,
+    ),
+    SVC(
+        kernel='rbf',
+        probability=True,
+        random_state=seed,
+    ),
+)
