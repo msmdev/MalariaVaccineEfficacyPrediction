02eb4a5a2261c8631a903b5db600403056309c63
Bernhard Reuter <bernhard-reuter@gmx.de>
2022-09-17 08:49:58 +0200

    Add RNCV results for thresholds in [0.1, 0.65].

---
+++ results/threshold0.3/RLR/selective/RLR_config.py
@@ -0,0 +1,27 @@
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from typing import Any, Dict, List
+from source.config import seed
+from numpy.random import RandomState
+
+
+# Set up grid of parameters to optimize over
+# 3*3=9 points
+param_grid: Dict[str, List[Any]] = {
+    'logisticregression__l1_ratio': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    'logisticregression__C': [1.e-4, 1.e-3, 1.e-2, 1.e-1, 1.e0, 1.e1, 1.e2, 1.e3, 1.e4],
+}
+
+estimator = make_pipeline(
+    StandardScaler(
+        with_mean=True,
+        with_std=True,
+    ),
+    LogisticRegression(
+        penalty='elasticnet',
+        solver='saga',
+        max_iter=10000,
+        random_state=RandomState(seed),
+    ),
+)
