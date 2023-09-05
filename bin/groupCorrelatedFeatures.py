54d23723cbf95dca3fa4404135558476b363d394
Bernhard Reuter <bernhard-reuter@gmx.de>
2023-09-05 17:31:22 +0200

    Add missing whitespace in warnings output.

--- bin/groupCorrelatedFeatures.py
+++ bin/groupCorrelatedFeatures.py
@@ -44,7 +44,6 @@ def main(
     correlation_threshold: float,
     correlation_method: str,
 ):
-
     # some input sanity checks:
     if not 0.0 <= correlation_threshold <= 1.0:
         raise ValueError("correlation_threshold is not in [0, 1]")
@@ -64,7 +63,7 @@ def main(
 
     if correlation_threshold == 1.0:
         print(
-            f"Correlation grouping with threshold {correlation_threshold} requested."
+            f"Correlation grouping with threshold {correlation_threshold} requested. "
             "Since 1.0 is the maximal possible correlation, the data will be saved unchanged.\n"
         )
     else:
@@ -224,7 +223,6 @@ def main(
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
         description=("Function to group strongly covarying features together.")
     )
